3c16fa8c2f012b8f823584450bc3135df6f20a0c
Xianfeng Yuan <xianfengyuan@yahoo.com>
2015-12-07 14:59:54 -0800

    attribute take no dash in :attr format

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,2 +1,2 @@
-default.jira-menlo[:java][:jdk_version] = "1.8.0_66"
-default.jira-menlo[:java][:download_path] = "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.rpm"
+default['jira-menlo']['java']['jdk_version'] = "1.8.0_66"
+default['jira-menlo']['java']['download_path'] = "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.rpm"
