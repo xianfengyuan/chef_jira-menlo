f6579bab109299de66c57dd65ec34a153ad0e59a
Xianfeng Yuan <xianfengyuan@yahoo.com>
2015-12-07 18:59:06 -0800

    provide fqdn

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 default.jira[:version]          = "7.0.2"
 default.jira[:install_type]     = "standalone"
+
+default['jira-menlo']['apache2']['fqdn'] = "jira.menlosecurity.com"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 node.override[:jira][:jvm][:minimum_memory] = "1024m"
 node.override[:jira][:jvm][:maximum_memory] = "#{(node.memory.total.to_i * 0.45 ).floor / 1024}m"
 node.override[:jira][:jvm][:maximum_permgen] = "#{(node.memory.total.to_i * 0.15).floor / 1024}m"
+node.override[:jira][:apache2][:virtual_host_name] = "#{node['jira-menlo']['apache2']['fqdn']}"
 
 package "sysstat" do
   action :install
