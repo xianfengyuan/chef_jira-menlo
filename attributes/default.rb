84e54f0099f0f2acb45a64309ee91d912e9fef87
Xianfeng Yuan <xianfengyuan@yahoo.com>
2015-12-08 00:24:55 -0800

    upgrade to 7.0.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
-default.jira[:version]          = "7.0.2"
+default.jira[:version]          = "7.0.4"
 default.jira[:install_type]     = "standalone"
+default.jira[:url] = "https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-#{node['jira']['flavor']}-#{node['jira']['version']}-jira-#{node['jira']['version']}.tar.gz"
+default.jira[:checksum] = "234f66679425c2285a68d75c877785d186cc7b532d73ada0d6907d67833e1522"
 
 default['jira-menlo']['apache2']['fqdn'] = "jira.menlosecurity.com"
