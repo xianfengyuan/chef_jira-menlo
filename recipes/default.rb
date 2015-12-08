93775fedd1e9ea2dd03d46720854394eebca54a0
Xianfeng Yuan <xianfengyuan@yahoo.com>
2015-12-07 18:19:01 -0800

    fix jvm memory error

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 node.override[:jira][:jvm][:minimum_memory] = "1024m"
-node.override[:jira][:jvm][:maximum_memory_memory] = "#{(node.memory.total.to_i * 0.45 ).floor / 1024}m"
+node.override[:jira][:jvm][:maximum_memory] = "#{(node.memory.total.to_i * 0.45 ).floor / 1024}m"
 node.override[:jira][:jvm][:maximum_permgen] = "#{(node.memory.total.to_i * 0.15).floor / 1024}m"
 
 package "sysstat" do
