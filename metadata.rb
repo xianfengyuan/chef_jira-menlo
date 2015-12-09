4f4e3c49b973030654338e041ebfcb68f8c00d3d
Xianfeng Yuan <xianfengyuan@yahoo.com>
2015-12-08 23:26:43 -0800

    remove unused dependency

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      "Installs/Configures jira-menlo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.1"
 
-depends "apache2"
 depends "jira"
